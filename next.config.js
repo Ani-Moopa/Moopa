4a85c5e18b36a8e9398be84ce8a440ea90062143
Judokasarin <officialmadarato@gmail.com>
2023-08-13 12:05:51 +0530

    🪰🪲🐞🦗🪳🐛

--- next.config.js
+++ next.config.js
@@ -28,26 +28,26 @@ module.exports = withPWA({
         destination: '/en',
         permanent: true,
       },
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://madara.to",
-          }, // replace this your actual origin
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
+      // {
+      //   // matching all API routes
+      //   source: "/api/:path*",
+      //   headers: [
+      //     { key: "Access-Control-Allow-Credentials", value: "true" },
+      //     {
+      //       key: "Access-Control-Allow-Origin",
+      //       value: "https://madara.to",
+      //     }, // replace this your actual origin
+      //     {
+      //       key: "Access-Control-Allow-Methods",
+      //       value: "GET,DELETE,PATCH,POST,PUT",
+      //     },
+      //     {
+      //       key: "Access-Control-Allow-Headers",
+      //       value:
+      //         "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      //     },
+      //   ],
+      // },
       // {
       //   source: "/(.*)",
       //   headers: createSecureHeaders({
