6d96bdf3f64f26e0451867ccac041ce2b318c6d5
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2023-09-16 13:35:01 -0500

    Update [id].js

--- pages/en/manga/[id].js
+++ pages/en/manga/[id].js
@@ -32,7 +32,7 @@ export default function Manga({ info, userManga }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
-          content={`Moopa - ${info.title.romaji || info.title.english}`}
+          content={`Shizuru - ${info.title.romaji || info.title.english}`}
         />
         <meta
           name="twitter:description"
