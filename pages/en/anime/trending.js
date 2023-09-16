b038e99ba18f10e86775aa5b5c740bef6847327f
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2023-09-16 13:35:44 -0500

    Update trending.js

--- pages/en/anime/trending.js
+++ pages/en/anime/trending.js
@@ -97,11 +97,11 @@ export default function TrendingAnime({ sessions }) {
   return (
     <Fragment>
       <Head>
-        <title>Moopa - Trending Anime</title>
+        <title>Shizuru - Trending Anime</title>
         <meta name="title" content="Trending Anime" />
         <meta
           name="description"
-          content="Explore Top Trending Anime - Dive into the latest and most popular anime series on Moopa. From thrilling action to heartwarming romance, discover the buzzworthy shows that have everyone talking. Stream now and stay up-to-date with the hottest anime trends!"
+          content="Explore Top Trending Anime - Dive into the latest and most popular anime series on Shizuru. From thrilling action to heartwarming romance, discover the buzzworthy shows that have everyone talking. Stream now and stay up-to-date with the hottest anime trends!"
         />
       </Head>
       <MobileNav sessions={sessions} />
