a83b1d44079193ea5bcfc26a56ecf8926e96b1b6
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2023-09-16 12:41:43 -0500

    Update index.js

--- pages/en/index.js
+++ pages/en/index.js
@@ -279,15 +279,15 @@ export default function Home({ detail, populars, upComing }) {
   return (
     <Fragment>
       <Head>
-        <title>Moopa</title>
+        <title>Shizuru</title>
         <meta charSet="UTF-8"></meta>
         <link rel="icon" href="/svg/c.svg" />
-        <link rel="canonical" href="https://moopa.live/en/" />
+        <link rel="canonical" href="https://Shizuru.live/en/" />
         <meta name="twitter:card" content="summary_large_image" />
         {/* Write the best SEO for this homepage */}
         <meta
           name="description"
-          content="Discover your new favorite anime or manga title! Moopa offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Moopa today!"
+          content="Discover your new favorite anime or manga title! Shizuru offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Shizuru today!"
         />
         <meta
           name="keywords"
@@ -297,25 +297,25 @@ export default function Home({ detail, populars, upComing }) {
         <meta name="robots" content="index, follow" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://moopa.live/" />
+        <meta property="og:url" content="https://Shizuru.live/" />
         <meta
           property="og:title"
-          content="Moopa - Free Anime and Manga Streaming"
+          content="Shizuru - Free Anime and Manga Streaming"
         />
         <meta
           property="og:description"
-          content="Discover your new favorite anime or manga title! Moopa offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Moopa today!"
+          content="Discover your new favorite anime or manga title! Shizuru offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Shizuru today!"
         />
         <meta property="og:image" content="/preview.png" />
-        <meta property="og:site_name" content="Moopa" />
+        <meta property="og:site_name" content="Shizuru" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
-          content="Moopa - Free Anime and Manga Streaming"
+          content="Shizuru - Free Anime and Manga Streaming"
         />
         <meta
           name="twitter:description"
-          content="Discover your new favorite anime or manga title! Moopa offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Moopa today!"
+          content="Discover your new favorite anime or manga title! Shizuru offers a vast library of high-quality content, accessible on multiple devices and without any interruptions. Start using Shizuru today!"
         />
         <meta name="twitter:image" content="/preview.png" />
       </Head>
