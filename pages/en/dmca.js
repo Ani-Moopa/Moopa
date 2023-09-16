d9443fc5a85f82e7cdb38102a31872a3a7654e20
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2023-09-16 13:20:25 -0500

    Update dmca.js

--- pages/en/dmca.js
+++ pages/en/dmca.js
@@ -5,12 +5,12 @@ export default function DMCA() {
   return (
     <>
       <Head>
-        <title>Moopa - DMCA</title>
+        <title>Shizuru - DMCA</title>
         <meta name="DMCA" content="DMCA" />
         <meta property="og:title" content="DMCA" />
         <meta
           property="og:description"
-          content="Moopa.live is committed to respecting the intellectual
+          content="Shizuru is committed to respecting the intellectual
                   property rights of others and complying with the Digital
                   Millennium Copyright Act (DMCA)."
         />
@@ -29,7 +29,7 @@ export default function DMCA() {
             <div className="flex flex-col gap-10">
               <div className="flex flex-col gap-3 text-[#cdcdcd]">
                 <p>
-                  Moopa.live is committed to respecting the intellectual
+                  Shizuru is committed to respecting the intellectual
                   property rights of others and complying with the Digital
                   Millennium Copyright Act (DMCA). We take copyright
                   infringement seriously and will respond to notices of alleged
@@ -81,19 +81,19 @@ export default function DMCA() {
               </p>
               <h1 className="text-white font-karla">DISCLAIMER:</h1>
               <p className="text-[#cdcdcd]">
-                None of the files listed on Moopa.live are hosted on our
+                None of the files listed on Shizuru are hosted on our
                 servers. All links point to content hosted on third-party
-                websites. Moopa.live does not accept responsibility for content
+                websites. .Shizuru does not accept responsibility for content
                 hosted on third-party websites and has no involvement in the
                 downloading/uploading of movies. We only post links that are
                 available on the internet. If you believe that any content on
                 our website infringes upon your intellectual property rights and
                 you hold the copyright for that content, please report it to{" "}
                 <a
-                  href="mailto:contact@moopa.live?subject=[Moopa]%20-%20Your%20Subject"
+                  href="mailto:contact@rawflyanime.com?subject=[Shizuru]%20-%20Your%20Subject"
                   className="font-semibold"
                 >
-                  contact@moopa.live
+                  contact@rawflyanime.com
                 </a>{" "}
                 and the content will be immediately removed.
               </p>
