c006231ad81cadf4fce2df27269f9ec7e8d287b5
A i j a Z <72906651+Aijazmakerb@users.noreply.github.com>
2023-09-23 18:31:15 +0530

    Update components/anime/charactersCard.js
    Co-authored-by: Factiven <factiven.org@gmail.com>

--- components/anime/charactersCard.js
+++ components/anime/charactersCard.js
@@ -17,7 +17,7 @@ export default function Characters({ info }) {
                 )}
             </div>
             <div className="grid w-full grid-cols-1 gap-[12px] md:gap-4 md:grid-cols-3 md:pt-7 md:pb-5 px-3 md:px-5 pt-4">
-                {info.slice(0, showAll ? info.length : 6).map((item, index) => {
+                {info?.slice(0, showAll ? info.length : 6).map((item, index) => {
                     return <a key={index} className="md:hover:scale-[1.02] snap-start hover:shadow-lg scale-100 transition-transform duration-200 ease-out w-full cursor-default">
                         <div className="text-gray-300 space-x-4 col-span-1 flex w-full h-24 bg-secondary rounded-md">
                             <div className="relative h-full w-16">
