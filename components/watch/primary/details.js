420829cef2db84d42b720a37ac2771d145eed703
Artrix9095 <sefu786@outlook.com>
2023-11-22 18:56:48 -0800

    Make details cover lead back to anime page

--- components/watch/primary/details.js
+++ components/watch/primary/details.js
@@ -3,6 +3,7 @@ import { useAniList } from "../../../lib/anilist/useAnilist";
 import Skeleton from "react-loading-skeleton";
 import DisqusComments from "../../disqus";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Details({
   info,
@@ -40,13 +41,18 @@ export default function Details({
       <div className="pb-4 h-full flex">
         <div className="aspect-[9/13] h-[240px]">
           {info ? (
-            <img
-              src={info.coverImage.extraLarge}
-              alt="Anime Cover"
-              width={1000}
-              height={1000}
-              className="object-cover aspect-[9/13] h-[240px] rounded-md"
-            />
+            <Link
+              className="hover:scale-105 hover:shadow-lg duration-300 ease-out"
+              href={`/en/anime/${id}`}
+            >
+              <img
+                src={info.coverImage.extraLarge}
+                alt="Anime Cover"
+                width={1000}
+                height={1000}
+                className="object-cover aspect-[9/13] h-[240px] rounded-md"
+              />
+            </Link>
           ) : (
             <Skeleton height={240} />
           )}
